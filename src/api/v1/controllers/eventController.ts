f7e521feb7f73d02b349d08711956b5a9b79516b
Chris Feasby <cfeasby@academic.rrc.ca>
2026-02-04 13:16:32 -0600

    Add event routes to app.ts and not implemented for controllers so app will work

---
+++ src/api/v1/controllers/eventController.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from "express";
+
+export function createEvent(req: Request, res: Response){
+  res.status(501).json({ message: "Not implemented yet" });
+};
+
+export function getAllEvents(req: Request, res: Response) {
+  res.status(501).json({ message: "Not implemented yet" });
+};
+
+export function getEventById(req: Request, res: Response) {
+  res.status(501).json({ message: "Not implemented yet" });
+};
+
+export function updateEvent(req: Request, res: Response) {
+  res.status(501).json({ message: "Not implemented yet" });
+};
+
+export function deleteEvent(req: Request, res: Response) {
+  res.status(501).json({ message: "Not implemented yet" });
+};
--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import healthRoutes from "./api/v1/routes/healthRoutes";
+import eventRoutes from "./api/v1/routes/eventRoutes";
 
 const app = express();
 
 app.use(express.json());
-
+app.use("/api/v1", eventRoutes);
 app.use("/api/v1", healthRoutes);
 
 // Define a route
