472e29eac2bb40c5730118af71da4bc4832e0e85
Chris Feasby <cfeasby@academic.rrc.ca>
2026-02-05 11:33:39 -0600

    Update eventRoutes with better description

--- src/api/v1/routes/eventRoutes.ts
+++ src/api/v1/routes/eventRoutes.ts
@@ -11,15 +11,23 @@ import {
 
 const router = Router();
 
+// POST /api/v1/events - Create an event
 router.post(
     "/events",
     validateRequest(eventSchemas.create),
     createEvent
 );
 
+// GET /api/v1/events - Get all events
 router.get("/events", getAllEvents);
+
+// GET /api/v1/events/:id - Get event by Id
 router.get("/events/:id", getEventById);
+
+// PUT /api/v1/events/:id - Update event by Id
 router.put("/events/:id", updateEvent);
+
+// DELETE /api/v1/events/:id - Delete event by Id
 router.delete("/events/:id", deleteEvent);
 
 export default router;
