31b493dc1ba94bf09da04687b688f57bce5624b1
Chris Feasby <cfeasby@academic.rrc.ca>
2026-02-05 10:43:03 -0600

    Add FirestoreDataTypes interface

---
+++ src/api/v1/types/firestore.ts
@@ -0,0 +1,9 @@
+export interface FirestoreDataTypes {
+    stringValue?: string;
+    numberValue?: number;
+    booleanValue?: boolean;
+    nullValue?: null;
+    dateValue?: Date;
+    objectValue?: Record<string, any>;
+    arrayValue?: Array<any>;
+};
